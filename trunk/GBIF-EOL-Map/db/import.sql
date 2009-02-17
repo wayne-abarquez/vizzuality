4da3ef11d72294156107e554dc39d1889f12edff
timrobertson100 <timrobertson100@07d80358-b58a-11dd-8d2d-9519526c4844>
2009-02-17 15:37:52 +0000

    Scripts to process data

---
+++ trunk/GBIF-EOL-Map/db/import.sql
@@ -0,0 +1,4 @@
+load data infile '/Users/tim/dev/ibiodiversity/GBIF-EOL-Map/db/dump/eol_data_resource.txt' into table data_resource;
+load data infile '/Users/tim/dev/ibiodiversity/GBIF-EOL-Map/db/dump/eol_data_provider.txt' into table data_provider;
+load data infile '/Users/tim/dev/ibiodiversity/GBIF-EOL-Map/db/dump/eol_taxon_resource.txt' into table taxon_resource;
+load data infile '/Users/tim/dev/ibiodiversity/GBIF-EOL-Map/db/dump/eol_taxon_site_processed.txt' into table taxon_site;
