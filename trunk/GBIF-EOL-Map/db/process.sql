d5051135c85f479bbbcccf3654d70fa5da5bfd00
timrobertson100 <timrobertson100@07d80358-b58a-11dd-8d2d-9519526c4844>
2009-03-14 11:50:18 +0000

    work in progress on tiling for single occurrences with Javi

--- trunk/GBIF-EOL-Map/db/process.sql
+++ trunk/GBIF-EOL-Map/db/process.sql
@@ -20,8 +20,6 @@ truncate table tile_16_taxon;
 truncate table tile_17_taxon;
 truncate table tile_18_taxon;
 truncate table tile_19_taxon;
-truncate table tile_20_taxon;
-truncate table tile_21_taxon;
 
 insert into tile_0_taxon
 select tile4_x, tile4_y, taxon_id, sum(num_occ), tile0_x, tile0_y
@@ -29,107 +27,96 @@ from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_1_taxon
-select tile4_x, tile4_y, taxon_id, sum(num_occ), tile1_x, tile1_y
+select tile5_x, tile5_y, taxon_id, sum(num_occ), tile1_x, tile1_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_2_taxon
-select tile4_x, tile4_y, taxon_id, sum(num_occ), tile2_x, tile2_y
+select tile6_x, tile6_y, taxon_id, sum(num_occ), tile2_x, tile2_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_3_taxon
-select tile5_x, tile5_y, taxon_id, sum(num_occ), tile3_x, tile3_y
+select tile7_x, tile7_y, taxon_id, sum(num_occ), tile3_x, tile3_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_4_taxon
-select tile6_x, tile6_y, taxon_id, sum(num_occ), tile4_x, tile4_y
+select tile8_x, tile8_y, taxon_id, sum(num_occ), tile4_x, tile4_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_5_taxon
-select tile7_x, tile7_y, taxon_id, sum(num_occ), tile5_x, tile5_y
+select tile9_x, tile9_y, taxon_id, sum(num_occ), tile5_x, tile5_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_6_taxon
-select tile8_x, tile8_y, taxon_id, sum(num_occ), tile6_x, tile6_y
+select tile10_x, tile10_y, taxon_id, sum(num_occ), tile6_x, tile6_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert ignore into tile_7_taxon
-select tile9_x, tile9_y, taxon_id, sum(num_occ), tile7_x, tile7_y
+select tile11_x, tile11_y, taxon_id, sum(num_occ), tile7_x, tile7_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_8_taxon
-select tile10_x, tile10_y, taxon_id, sum(num_occ), tile8_x, tile8_y
+select tile12_x, tile12_y, taxon_id, sum(num_occ), tile8_x, tile8_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_9_taxon
-select tile11_x, tile11_y, taxon_id, sum(num_occ), tile9_x, tile9_y
+select tile13_x, tile13_y, taxon_id, sum(num_occ), tile9_x, tile9_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_10_taxon
-select tile12_x, tile12_y, taxon_id, sum(num_occ), tile10_x, tile10_y
+select tile14_x, tile14_y, taxon_id, sum(num_occ), tile10_x, tile10_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_11_taxon
-select tile13_x, tile13_y, taxon_id, sum(num_occ), tile11_x, tile11_y
+select tile15_x, tile15_y, taxon_id, sum(num_occ), tile11_x, tile11_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_12_taxon
-select tile14_x, tile14_y, taxon_id, sum(num_occ), tile12_x, tile12_y
+select tile16_x, tile16_y, taxon_id, sum(num_occ), tile12_x, tile12_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_13_taxon
-select tile15_x, tile15_y, taxon_id, sum(num_occ), tile13_x, tile13_y
+select tile17_x, tile17_y, taxon_id, sum(num_occ), tile13_x, tile13_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_14_taxon
-select tile16_x, tile16_y, taxon_id, sum(num_occ), tile14_x, tile14_y
+select tile18_x, tile18_y, taxon_id, sum(num_occ), tile14_x, tile14_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_15_taxon
-select tile17_x, tile17_y, taxon_id, sum(num_occ), tile15_x, tile15_y
+select tile19_x, tile19_y, taxon_id, sum(num_occ), tile15_x, tile15_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_16_taxon
-select tile18_x, tile18_y, taxon_id, sum(num_occ), tile16_x, tile16_y
+select tile20_x, tile20_y, taxon_id, sum(num_occ), tile16_x, tile16_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_17_taxon
-select tile19_x, tile19_y, taxon_id, sum(num_occ), tile17_x, tile17_y
+select tile21_x, tile21_y, taxon_id, sum(num_occ), tile17_x, tile17_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_18_taxon
-select tile20_x, tile20_y, taxon_id, sum(num_occ), tile18_x, tile18_y
+select tile22_x, tile22_y, taxon_id, sum(num_occ), tile18_x, tile18_y
 from taxon_site
 group by 1,2,5,6,3;
 
 insert into tile_19_taxon
-select tile21_x, tile21_y, taxon_id, sum(num_occ), tile19_x, tile19_y
+select tile23_x, tile23_y, taxon_id, sum(num_occ), tile19_x, tile19_y
 from taxon_site
 group by 1,2,5,6,3;
-
-insert into tile_20_taxon
-select tile22_x, tile22_y, taxon_id, sum(num_occ), tile20_x, tile20_y
-from taxon_site
-group by 1,2,5,6,3;
-
-insert into tile_21_taxon
-select tile23_x, tile23_y, taxon_id, sum(num_occ), tile21_x, tile21_y
-from taxon_site
-group by 1,2,5,6,3;
-
