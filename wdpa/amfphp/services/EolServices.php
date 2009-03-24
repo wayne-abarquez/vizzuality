5ae8763de9cc6fb04f3424a10a462f4126003970
jatorre <jatorre@gmail.com>
2009-03-24 10:57:17 +0000

    AMFPHP added to the project

---
+++ wdpa/amfphp/services/EolServices.php
@@ -0,0 +1,22 @@
+<?php
+
+class EolServices {
+    public function getProviders($taxonId) {
+        $link = mysql_connect('localhost:3307', 'root', 'root');
+        mysql_select_db('eol', $link) or die('Could not select database.');
+        $sql="select dr.name as data_resource_name, dp.name as data_provider_name,data_provider_id,data_resource_id,1 as num_occ from taxon_resource tr left join data_provider dp on tr.data_provider_id= dp.id left join data_resource dr on dr.id=tr.data_resource_id where taxon_id=".$taxonId;
+
+        $result = mysql_query($sql);
+        
+        $output=array();
+        while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+            $output[]=$row;
+        }
+        return $output;        
+	}
+
+	public function getPaDetails($paId) {
+		return "Hi";
+	}
+}
+?>
