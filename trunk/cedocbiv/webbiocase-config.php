62fe653a8643e2e4055057c109b726b8f17348fa
fmr1983 <fmr1983@07d80358-b58a-11dd-8d2d-9519526c4844>
2009-11-30 13:17:06 +0000

    first import

---
+++ trunk/cedocbiv/webbiocase-config.php
@@ -0,0 +1,13 @@
+<?php
+/** The name of the database for CeDocBiV */
+define('DB_NAME', 'cormofitos');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'pepe');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+?>
