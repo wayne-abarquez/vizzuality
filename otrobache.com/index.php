d0e53e7756a9843a1112667d211b15134595fc75
jatorre <jatorre@07d80358-b58a-11dd-8d2d-9519526c4844>
2010-03-21 12:11:48 +0000

    php home

--- otrobache.com/index.php
+++ otrobache.com/index.php
@@ -12,19 +12,47 @@
 	  <link rel="stylesheet" href="/stylesheets/layout.css" type="text/css">
 
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-		<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
-		<script type="text/javascript" src="/javascript/init.js"></script>	
+        <script type="text/javascript" src="http://maps.google.com/maps?gwt=1&amp;file=api&amp;sensor=false&amp;v=2.x&amp;key=ABQIAAAAtDJGVn6RztUmxjnX5hMzjRQbotjeVUwZe96Bn7L2MEyZ6pftAxRtcuen6T4sYnXsSUQ0C78UJslfeA">
+    <script type="text/javascript" src="/javascript/init.js"></script>	
+    <script type="text/javascript">
+    function initialize() {
+      if (GBrowserIsCompatible()) {
 
-    	
+          var options = {
+
+          onIdleCallback : function() { alert("search control is idle");},
+          onGenerateMarkerHtmlCallback : function(marker, div, result) { div.innerHTML = "<input type='button' value='Anadir este bache' />"; return div; },
+          searchFormHint : "busca direccion"
+          };      
+
+        var map = new GMap2(document.getElementById("map_canvas"),{googleBarOptions: options});
+        map.setCenter(new google.maps.LatLng(40.4392758681028,-3.7121450901031494),12);
+        var l = new GLayer("ft:136993");
+        l.setParameter("h", "true");
+        l.setParameter("s", "select+address+from+136993");
+
+        map.addOverlay(l);
+        //map.setUIToDefault();
+        map.enableScrollWheelZoom();
+        map.addControl(new GMapTypeControl());
+        //map.addControl(new GLargeMapControl());
+        //map.setUIToDefault();
+
+
+        //map.enableGoogleBar();
+
+      }
+    }
+    </script>	
 	</head>
 
-	<body>
+	<body onload="initialize()" onunload="GUnload()">
 		<div id="header">
 			<p><a href="#">Un artículo de El País</a> nos impulsó a hacer esto. En madrid se realizan mas de 90 denuncia al día, sobre los socabones de la capital. Por desgracia estos datos no son públicos, y por eso queremos saber dónde y cuantos realmente hay</p>
 		</div>
 		
 		<div id="content">
-			<div id="map">
+			<div id="map_canvas" style="width:699px; height:372px;">
 			</div>
 			<img src="../images/otrobache.png" />
 			<p><span>más de 567.123 baches reportados, <a href="#">reporta uno</a></span></p>
