f0680c14009e80c07222a379b4048edb9b866883
timrobertson100 <timrobertson100@07d80358-b58a-11dd-8d2d-9519526c4844>
2010-12-10 04:35:21 +0000

    Did a bit of styling.  Boundaries are an issue of course... but surmountable issue I think

--- jtileserver/src/main/java/com/vizzuality/map/tileserver/util/PNGWriter.java
+++ jtileserver/src/main/java/com/vizzuality/map/tileserver/util/PNGWriter.java
@@ -52,28 +52,93 @@ public class PNGWriter<T> implements MessageBodyWriter<T> {
 				// get the pixel offset for the X,Y in this tile
 				//int offsetX = getTileOffset(tdc.getClusterX(), zoomLookAhead, pixelsPerCell);
 				//int offsetY = getTileOffset(tdc.getClusterY(), zoomLookAhead, pixelsPerCell);
-				int offsetX = tdc.getClusterX();
-				int offsetY = tdc.getClusterY();
+				int x = tdc.getClusterX();
+				int y = tdc.getClusterY();
 				
 				// System.out.println("Offset x[" + offsetX + "] y[" +offsetY + "]");
 				
-				// determine the starting draw position
-				int cellStart = (offsetY*tileSize)+(offsetX);
-				int colorIndex = 0;
+				//draw the cells
+				try {
+					
+					// center cell
+					rasterData[x + (256*y)] = 0xFFFF6666;
+					
+					// 5x5 box around the center
+					for (int i=x-2; i<=x+2; i++) {
+						for (int j=y-2; j<=y+2; j++) {
+							draw(rasterData, 0xFFFF6666, i + (256*j));
+						}
+					}
+					
+					// black perimeter
+					draw(rasterData, 0xFF000000, x-4 + (256*(y-1)));
+					draw(rasterData, 0xFF000000, x-4 + (256*(y)));
+					draw(rasterData, 0xFF000000, x-4 + (256*(y+1)));
+					draw(rasterData, 0xFF000000, x+4 + (256*(y-1)));
+					draw(rasterData, 0xFF000000, x+4 + (256*(y)));
+					draw(rasterData, 0xFF000000, x+4 + (256*(y+1)));
+					draw(rasterData, 0xFF000000, x-1 + (256*(y-4)));
+					draw(rasterData, 0xFF000000, x + (256*(y-4)));
+					draw(rasterData, 0xFF000000, x+1 + (256*(y-4)));
+					draw(rasterData, 0xFF000000, x-1 + (256*(y+4)));
+					draw(rasterData, 0xFF000000, x + (256*(y+4)));
+					draw(rasterData, 0xFF000000, x+1 + (256*(y+4)));					
+					
+					// corner
+					draw(rasterData, 0xFF000000, x-3 + (256*(y-3)));
+					draw(rasterData, 0xFF000000, x+3 + (256*(y-3)));
+					draw(rasterData, 0xFF000000, x-3 + (256*(y+3)));
+					draw(rasterData, 0xFF000000, x+3 + (256*(y+3)));
+					// rounded edge of black perimeter
+					draw(rasterData, 0xFF1A1A1A, x-4 + (256*(y-2)));
+					draw(rasterData, 0xFF1A1A1A, x-2 + (256*(y-4)));
+					draw(rasterData, 0xFF1A1A1A, x+2 + (256*(y-4)));
+					draw(rasterData, 0xFF1A1A1A, x+4 + (256*(y-2)));
+					draw(rasterData, 0xFF1A1A1A, x+4 + (256*(y+2)));
+					draw(rasterData, 0xFF1A1A1A, x+2 + (256*(y+4)));
+					draw(rasterData, 0xFF1A1A1A, x-2 + (256*(y+4)));
+					draw(rasterData, 0xFF1A1A1A, x-4 + (256*(y+2)));
+					
+					// between center and perimeter
+					draw(rasterData, 0xFF552222, x-2 + (256*(y-3)));
+					draw(rasterData, 0xFFAA4444, x-1 + (256*(y-3)));
+					draw(rasterData, 0xFFCC5555, x + (256*(y-3)));
+					draw(rasterData, 0xFFAA4444, x+1 + (256*(y-3)));
+					draw(rasterData, 0xFF552222, x+2 + (256*(y-3)));
+					draw(rasterData, 0xFF552222, x-2 + (256*(y+3)));
+					draw(rasterData, 0xFFAA4444, x-1 + (256*(y+3)));
+					draw(rasterData, 0xFFCC5555, x + (256*(y+3)));
+					draw(rasterData, 0xFFAA4444, x+1 + (256*(y+3)));
+					draw(rasterData, 0xFF552222, x+2 + (256*(y+3)));
+					draw(rasterData, 0xFF552222, x-3 + (256*(y-2)));
+					draw(rasterData, 0xFFAA4444, x-3 + (256*(y-1)));
+					draw(rasterData, 0xFFCC5555, x-3 + (256*(y)));
+					draw(rasterData, 0xFFAA4444, x-3 + (256*(y+1)));
+					draw(rasterData, 0xFF552222, x-3 + (256*(y+2)));
+					draw(rasterData, 0xFF552222, x+3 + (256*(y-2)));
+					draw(rasterData, 0xFFAA4444, x+3 + (256*(y-1)));
+					draw(rasterData, 0xFFCC5555, x+3 + (256*(y)));
+					draw(rasterData, 0xFFAA4444, x+3 + (256*(y+1)));
+					draw(rasterData, 0xFF552222, x+3 + (256*(y+2)));
+
+					
+				} catch (RuntimeException e) {
+				}
 				
 				// for the number of rows in the cell
-	        	for (int i=0; i<pixelsPerCell; i++) {
-	        		// draw the cells
-	        		for (int j=cellStart; j<cellStart+pixelsPerCell; j++) {
-	        			try {
-							rasterData[j] = colorsAsInt[colorIndex];
-						} catch (RuntimeException e) {
-							log.error(e.getMessage(), e);
-						}
-	        		}
-	        		cellStart+=tileSize;
-	        	}							
+//	        	for (int i=0; i<pixelsPerCell; i++) {
+//	        		// draw the cells
+//	        		for (int j=cellStart; j<cellStart+pixelsPerCell; j++) {
+//	        			try {
+//							rasterData[j] = colorsAsInt[colorIndex];
+//						} catch (RuntimeException e) {
+//							log.error(e.getMessage(), e);
+//						}
+//	        		}
+//	        		cellStart+=tileSize;
+//	        	}							
 			}
+			
 			
 			BufferedImage bufferedImage = new BufferedImage(tileSize, tileSize, Transparency.TRANSLUCENT);
 			WritableRaster raster = bufferedImage.getWritableTile(0,0);
@@ -82,6 +147,11 @@ public class PNGWriter<T> implements MessageBodyWriter<T> {
 			byte[] png = encoder.pngEncode();
 			entityStream.write(png);
 		}
+	}
+
+	private void draw(int[] rasterData, int col, int index) {
+		if (index<rasterData.length)
+			rasterData[index] = col;
 	}
 	
 	public long getSize(T t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
@@ -103,12 +173,21 @@ public class PNGWriter<T> implements MessageBodyWriter<T> {
 	// because Colors are cached internally in Java, we save MUCH time creating them first
 	// and stopping their garbage collection (300msec to create a color on macbook pro)
 	protected static final Color[] colors = {
-		new Color(0x00, 0x00, 0xFF)
+		new Color(0xFF, 0x66, 0x66),
+		new Color(0x00, 0x00, 0x00),
+		new Color(0x1A, 0x1A, 0x1A),
+		new Color(0x55, 0x22, 0x22),
+		new Color(0xAA, 0x44, 0x44),
+		new Color(0xCC, 0x55, 0x55)
 	};
  
 	// the colors that are actually used (internally, Java will pick up a cached color from the 
 	// above cache)
 	protected static final int[] colorsAsInt = {
-		0xFF0000FF
-	};
-}
+		0xFFFF6666,
+		0xFF000000,
+		0xFF1A1A1A,
+		0xFF552222,
+		0xFFAA4444,
+		0xFFCC5555		
+	};}
